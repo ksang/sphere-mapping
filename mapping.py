9e8bdb334a98ec9a150104a276fd0cb3ba482ded
ksang <kaitoy@qq.com>
2023-08-25 18:36:06 +0800

    fix angles, improve graph

--- mapping.py
+++ mapping.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def naive_uv_to_sphere(uv: np.ndarray):
-    tp = np.empty((uv.shape))
-    tp[:, 0] = uv[:, 0] * 2 * np.pi
-    tp[:, 1] = uv[:, 1] * np.pi
-    return tp
+    phi_theta = np.empty((uv.shape))
+    phi_theta[:, 0] = uv[:, 0] * 2 * np.pi
+    phi_theta[:, 1] = uv[:, 1] * np.pi
+    return phi_theta
 
-def naive_sphere_to_uv(tp: np.ndarray):
-    uv = np.empty((tp.shape))
-    uv[:, 0] = tp[:, 0] / (2 * np.pi)
-    uv[:, 1] = tp[:, 1] / np.pi
+def naive_sphere_to_uv(phi_theta: np.ndarray):
+    uv = np.empty((phi_theta.shape))
+    uv[:, 0] = phi_theta[:, 0] / (2 * np.pi)
+    uv[:, 1] = phi_theta[:, 1] / np.pi
     return uv
--- sphere.py
+++ sphere.py
@@ -2,29 +2,30 @@ import numpy as np
 
 # Bijective projection between spherical and cartesian coordinates 
 # following convention: https://en.wikipedia.org/wiki/Spherical_coordinate_system
-def spherical_to_cartesian(angles: np.ndarray):
+def spherical_to_cartesian(phi_theta: np.ndarray):
     """
     Convert spherical angles to points on unit sphere in cartesian coordinate
-        Input:  angles, Nx2, (theta, phi), polar and azimuthal
+        Input:  phi_theta, Nx2, (phi, theta), azimuthal and polar 
         Output: points, Nx3, (x, y, z)
     """
-    xyz = np.empty((angles.shape[0], 3))
-    xyz[:, 0] = np.sin(angles[:, 0]) * np.cos(angles[:, 1]) 
-    xyz[:, 1] = np.sin(angles[:, 0]) * np.sin(angles[:, 1])
-    xyz[:, 2] = np.cos(angles[:, 0])
+    xyz = np.empty((phi_theta.shape[0], 3))
+    phi, theta = phi_theta[:, 0], phi_theta[:, 1]
+    xyz[:, 0] = np.sin(theta) * np.cos(phi) 
+    xyz[:, 1] = np.sin(theta) * np.sin(phi)
+    xyz[:, 2] = np.cos(theta)
     return xyz
 
 def cartesian_to_spherical(points: np.ndarray):
     """
     Convert points on unit sphere to spherical angles
         Input:  points, Nx3, (x, y, z) 
-        Output: angles, Nx2, (theta, phi), polar(0-pi) and azimuthal (0-2pi)
+        Output: phi_theta, Nx2, (phi, theta), azimuthal (0-2pi) and polar(0-pi) 
     """
-    tp = np.empty((points.shape[0], 2))
+    phi_theta = np.empty((points.shape[0], 2))
     points = points / np.linalg.norm(points)
-    tp[:, 0] = np.arccos(points[:, 2])
-    tp[:, 1] = np.arctan2(points[:, 1], points[:, 0]) + np.pi
-    return tp
+    phi_theta[:, 0] = np.arctan2(points[:, 1], points[:, 0]) + np.pi
+    phi_theta[:, 1] = np.arccos(points[:, 2])
+    return phi_theta
 
 def create_sphere_points(resolution: (int, int), mapping_fn):
     """
@@ -34,9 +35,10 @@ def create_sphere_points(resolution: (int, int), mapping_fn):
     """
     u = np.linspace(0., 1., resolution[0])
     v = np.linspace(0., 1., resolution[1])
-    pu, pv = np.meshgrid(u,v)
+    pu, pv = np.meshgrid(u,v, indexing='ij')
     grid = np.stack([pu, pv])
-    uv = np.moveaxis(grid, 0, -1).reshape(-1, 2)
-    angles = mapping_fn(uv)
-    points = spherical_to_cartesian(angles)
-    return points
+    uv = np.moveaxis(grid, 0, -1)
+    grid_shape = uv.shape[:2]
+    phi_theta = mapping_fn(uv.reshape(-1, 2))
+    points = spherical_to_cartesian(phi_theta)
+    return points.reshape(*grid_shape, 3)
