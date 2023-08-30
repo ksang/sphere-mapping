9880d6054443fb468cf419204abac8df3d8bd052
ksang <kaitoy@qq.com>
2023-08-30 18:03:07 +0800

    WIP: add neighbor logic

--- mapping.py
+++ mapping.py
@@ -1,13 +1,25 @@
 import numpy as np
 
-def naive_uv_to_sphere(uv: np.ndarray):
+def linear_uv_to_sphere(uv: np.ndarray):
     phi_theta = np.empty((uv.shape))
-    phi_theta[:, 0] = uv[:, 0] * 2 * np.pi
+    phi_theta[:, 0] = uv[:, 0] * 2 * np.pi - np.pi
     phi_theta[:, 1] = uv[:, 1] * np.pi
     return phi_theta
 
-def naive_sphere_to_uv(phi_theta: np.ndarray):
+def linear_sphere_to_uv(phi_theta: np.ndarray):
     uv = np.empty((phi_theta.shape))
-    uv[:, 0] = phi_theta[:, 0] / (2 * np.pi)
+    uv[:, 0] = (phi_theta[:, 0]) / (2 * np.pi) + 0.5
     uv[:, 1] = phi_theta[:, 1] / np.pi
+    return uv
+
+def uniform_area_uv_to_sphere(uv: np.ndarray):
+    phi_theta = np.empty(uv.shape)
+    phi_theta[:, 0] = uv[:, 0] * (2 * np.pi) - np.pi
+    phi_theta[:, 1] = np.arccos(-2 * uv[:, 1] + 1)
+    return phi_theta
+
+def uniform_area_sphere_to_uv(phi_theta: np.ndarray):
+    uv = np.empty(phi_theta.shape)
+    uv[:, 0] = phi_theta[:, 0] / (2 * np.pi) + 0.5
+    uv[:, 1] = (1.0 - np.cos(phi_theta[:, 1])) / 2.0
     return uv
--- sphere.py
+++ sphere.py
@@ -15,16 +15,18 @@ def spherical_to_cartesian(phi_theta: np.ndarray):
     xyz[:, 2] = np.cos(theta)
     return xyz
 
-def cartesian_to_spherical(points: np.ndarray):
+def cartesian_to_spherical(points: np.ndarray, eps: float = 1e-6):
     """
     Convert points on unit sphere to spherical angles
         Input:  points, Nx3, (x, y, z) 
         Output: phi_theta, Nx2, (phi, theta), azimuthal (0-2pi) and polar(0-pi) 
     """
     phi_theta = np.empty((points.shape[0], 2))
-    points = points / np.linalg.norm(points)
-    phi_theta[:, 0] = np.arctan2(points[:, 1], points[:, 0]) + np.pi
+    points / np.linalg.norm(points, axis=1).reshape(-1, 1)
     phi_theta[:, 1] = np.arccos(points[:, 2])
+
+    phi_theta[:, 0] = np.arctan2(points[:, 0], points[:, 1])
+    phi_theta[:, 0][ (np.abs(points[:, 0]) < eps) & (np.abs(points[:, 1]) < eps) ] = 0
     return phi_theta
 
 def create_sphere_points(resolution: (int, int), mapping_fn):
@@ -42,3 +44,24 @@ def create_sphere_points(resolution: (int, int), mapping_fn):
     phi_theta = mapping_fn(uv.reshape(-1, 2))
     points = spherical_to_cartesian(phi_theta)
     return points.reshape(*grid_shape, 3)
+
+def get_neighbor_vertices(points, sample, mapping_fn):
+    """
+    Find neighbor vertices of given sample
+        Input:  points, point grid in cartesian coordinates
+                sample, sample point in cartesian coordinates
+                mapping_fn, maps from spherical angles to linear uv plane
+        Output: 4 points on unit sphere that are the vertices
+    """
+    phi_num = points.shape[0]
+    theta_num = points.shape[1]
+    phi_theta = cartesian_to_spherical(sample.reshape(1, 3))
+    uv = mapping_fn(phi_theta).reshape(2)
+    u0 = int(uv[0] * phi_num)
+    if u0 == phi_num: u0 = 0
+    u1 = u0 + 1
+    if u1 == phi_num: u1 = 0
+    v0 = int(uv[1] * (theta_num - 1))
+    v1 = v0 + 1
+    if v1 == theta_num: v1 = theta_num - 1
+    return np.array([points[u0, v0], points[u1, v0], points[u0, v1], points[u1, v1]])
